e87173cfed5f06fd52506c278e7e3c4f139ba944
pupiesa <puppup.2547.2016@gmail.com>
2025-07-05 20:16:56 +0700

    enought for to day

---
+++ Python Project for Data Engineering/etl_project_gdp.py
@@ -0,0 +1,59 @@
+
+# Code for ETL operations on Country-GDP data
+
+
+# Importing the required libraries
+
+import pandas as pd
+import sqlite3
+from bs4 import BeautifulSoup
+from datetime import datetime
+import requests
+import numpy as np
+
+url = 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)'
+table_attribs = ['Country', 'GDP_USD_millions']
+db_name = 'World_Economies.db'
+table_name = 'Countries_by_GDP'
+csv_path = 'Countries_by_GDP.csv'
+html_page = requests.get(url).text
+data = BeautifulSoup(html_page, 'html.parser')
+df = pd.DataFrame(columns= table_attribs)
+table = data.find_all('tbody')
+rows = table[2].find_all('tr')
+df = pd.DataFrame(rows)
+print(df)
+def extract(url, table_attribs):
+    ''' This function extracts the required
+    information from the website and saves it to a dataframe. The
+    function returns the dataframe for further processing. '''
+    
+
+    return df
+
+def transform(df):
+    ''' This function converts the GDP information from Currency
+    format to float value, transforms the information of GDP from
+    USD (Millions) to USD (Billions) rounding to 2 decimal places.
+    The function returns the transformed dataframe.'''
+
+    return df
+
+def load_to_csv(df, csv_path):
+    ''' This function saves the final dataframe as a `CSV` file 
+    in the provided path. Function returns nothing.'''
+
+def load_to_db(df, sql_connection, table_name):
+    ''' This function saves the final dataframe as a database table
+    with the provided name. Function returns nothing.'''
+
+def run_query(query_statement, sql_connection):
+    ''' This function runs the stated query on the database table and
+    prints the output on the terminal. Function returns nothing. '''
+
+def log_progress(message):
+    ''' This function logs the mentioned message at a given stage of the code execution to a log file. Function returns nothing'''
+
+''' Here, you define the required entities and call the relevant 
+functions in the correct order to complete the project. Note that this
+portion is not inside any function.'''
