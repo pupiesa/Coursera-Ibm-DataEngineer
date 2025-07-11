fd4a5011149465d8de5979c2519ca7d60904e41f
pupiesa <puppup.2547.2016@gmail.com>
2025-07-11 20:03:38 +0700

    Refactor ETL script
    Refactored the ETL script for GDP data by cleaning up comments, improving extraction and transformation logic, and updating the data source URL to a web archive. Added logging, the ETL process execution, and implemented CSV/database loading and query execution with progress tracking.

--- Python Project for Data Engineering/etl_project_gdp.py
+++ Python Project for Data Engineering/etl_project_gdp.py
@@ -1,8 +1,3 @@
-# Code for ETL operations on Country-GDP data
-
-
-# Importing the required libraries
-
 import pandas as pd
 import sqlite3
 from bs4 import BeautifulSoup
@@ -10,64 +5,85 @@ from datetime import datetime
 import requests
 import numpy as np
 
-url = 'https://en.wikipedia.org/wiki/List_of_countries_by_GDP_(nominal)'
+url = 'https://web.archive.org/web/20230902185326/https://en.wikipedia.org/wiki/List_of_countries_by_GDP_%28nominal%29'
 table_attribs = ['Country', 'GDP_USD_millions']
 db_name = 'World_Economies.db'
 table_name = 'Countries_by_GDP'
 csv_path = 'Countries_by_GDP.csv'
 
 def extract(url, table_attribs):
-    # ''' This function extracts the required
-    # information from the website and saves it to a dataframe. The
-    # function returns the dataframe for further processing. '''
-    html_page = requests.get(url).text
-    data = BeautifulSoup(html_page, 'html.parser')
-    df = pd.DataFrame(columns= table_attribs)
-    table = data.find_all('tbody')
-    rows = table[2].find_all('tr')
+    page = requests.get(url).text
+    data = BeautifulSoup(page,'html.parser')
+    df = pd.DataFrame(columns=table_attribs)
+    tables = data.find_all('tbody')
+    rows = tables[2].find_all('tr')
     for row in rows:
-        cols = row.find_all('td')
-        if not cols:
-            continue
-        if not cols[0].find('a'):
-            continue
-        if cols[2].text.strip() == '—':
-            continue
-        data = {
-            'Country': cols[0].text.strip(), 
-            'GDP_USD_millions': cols[2].text.strip()
-            }
-
-        df = pd.concat([df,pd.DataFrame([data])], ignore_index = True)
-    
+        col = row.find_all('td')
+        if len(col)!=0:
+            if col[0].find('a') is not None and '—' not in col[2]:
+                data_dict = {"Country": col[0].a.contents[0],
+                             "GDP_USD_millions": col[2].contents[0]}
+                df1 = pd.DataFrame(data_dict, index=[0])
+                df = pd.concat([df,df1], ignore_index=True)
     return df
 
 def transform(df):
-    # ''' This function converts the GDP information from Currency
-    # format to float value, transforms the information of GDP from
-    # USD (Millions) to USD (Billions) rounding to 2 decimal places.
-    # The function returns the transformed dataframe.'''
-    df['GDP_USD_millions'] = df['GDP_USD_millions'].astype(float)
-    df['GDP_USD_millions'] = (df['GDP_USD_millions'] / 1000).round(2)
-    df = df.rename(columns={'GDP_USD_millions':'GDP_USD_billions'})
-    
+    GDP_list = df["GDP_USD_millions"].tolist()
+    GDP_list = [float("".join(x.split(','))) for x in GDP_list]
+    GDP_list = [np.round(x/1000,2) for x in GDP_list]
+    df["GDP_USD_millions"] = GDP_list
+    df=df.rename(columns = {"GDP_USD_millions":"GDP_USD_billions"})
     return df
 
 def load_to_csv(df, csv_path):
     # ''' This function saves the final dataframe as a `CSV` file 
     # in the provided path. Function returns nothing.'''
-    return df
+    df.to_csv(csv_path)
 def load_to_db(df, sql_connection, table_name):
     # ''' This function saves the final dataframe as a database table
     # with the provided name. Function returns nothing.'''
-    return df
+    df.to_sql(table_name, sql_connection ,if_exists='replace' ,index=False)
 def run_query(query_statement, sql_connection):
     # ''' This function runs the stated query on the database table and
     # prints the output on the terminal. Function returns nothing. '''
-    return query_statement
+    print(query_statement)
+    query_output = pd.read_sql(query_statement ,sql_connection)
+    print(query_output)
 def log_progress(message):
     # ''' This function logs the mentioned message at a given stage of the code execution to a log file. Function returns nothing'''
-    return message
+    timestamp_format = '%Y-%h-%d-%H:%M:%S' # Year-Monthname-Day-Hour-Minute-Second 
+    now = datetime.now() # get current timestamp 
+    timestamp = now.strftime(timestamp_format) 
+    with open("./etl_project_log.txt","a") as f: 
+        f.write(timestamp + ' : ' + message + '\n')
+    return
 # ''' Here, you define the required entities and call the relevant 
 # functions in the correct order to complete the project. Note that this
 # portion is not inside any function.'''
+
+log_progress('start extracting')
+
+df = extract(url,table_attribs)
+
+log_progress('transforming')
+
+transformed_data = transform(df)
+
+log_progress('load data to csv')
+
+load_to_csv(transformed_data, csv_path)
+log_progress('Data saved to CSV file')
+
+sql_connection = sqlite3.connect(db_name)
+
+log_progress('SQL Connection initiated.')
+
+load_to_db(transformed_data,sql_connection,table_name)
+
+log_progress('Data saved to database')
+query_statement = f"SELECT * from {table_name} WHERE GDP_USD_billions >= 100"
+run_query(query_statement, sql_connection)
+
+log_progress('Process Complete.')
+
+sql_connection.close()
